f33a5cca94f41087027c54a168bf484b45dedec0
ktk3376 <ktk3376@gmail.com>
2026-05-08 12:09:52 +0900

    first commit

---
+++ src/main/java/com/back/P14512Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P14512Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P14512Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/P14512ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P14512ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
