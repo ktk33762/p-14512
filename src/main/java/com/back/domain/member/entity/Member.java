ce6ea9fbdd6da00e65a782df5d4b9c3ea79e9e70
ktk3376 <ktk3376@gmail.com>
2026-05-11 12:49:32 +0900

    25강, SecurityConfig 설정

--- src/main/java/com/back/domain/member/entity/Member.java
+++ src/main/java/com/back/domain/member/entity/Member.java
@@ -1,17 +1,15 @@
 package com.back.domain.member.entity;
 
+import com.back.global.baseEntity.BaseEntity;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @NoArgsConstructor
-public class Member {
+public class Member extends BaseEntity {
     private String username;
     private String password;
     private String email;
-
 }
