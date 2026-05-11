0c4d81a1eaed96c7a7d10fe42f04d5e47417215f
ktk3376 <ktk3376@gmail.com>
2026-05-11 10:29:12 +0900

    16강, 타임리프 도입, value, 에러처리 개선

--- src/main/java/com/back/domain/controller/PostController.java
+++ src/main/java/com/back/domain/controller/PostController.java
@@ -10,11 +10,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.stream.Collectors;
 
@@ -24,59 +24,47 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts/write")
-    @ResponseBody
-    public String showWrite() {
-        return getWriteFormHtml("", "", "");
+    public String showWrite(WriteForm form, Model model) {
+        model.addAttribute("form", form);
+        return "post/post/write";
     }
 
     @AllArgsConstructor
     @Getter
     public static class WriteForm {
-        @NotBlank(message = "제목은 필수 입력입니다.")
-        @Size(min = 2, max = 20, message = "제목은 2자 이상 20자 이하로 입력해주세요.")
+        @NotBlank(message = "제목을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "제목은 2 ~ 10 자 이내로 입력해주세요.")
         String title;
 
-        @NotBlank(message = "내용은 필수 입력입니다.")
-        @Size(min = 2, max = 100, message = "내용은 2자 이상 100자 이하로 입력해주세요.")
+        @NotBlank(message = "내용을 입력해주세요.")
+        @Size(min = 2, max = 100, message = "내용은 2 ~ 100 자 이내로 입력해주세요.")
         String content;
     }
 
     @PostMapping("/posts/doWrite")
-    @ResponseBody
     @Transactional
     public String write(
-            @Valid WriteForm writeForm,
-            BindingResult bindingResult
+            @Valid WriteForm form,
+            BindingResult bindingResult,
+            Model model
     ) {
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             String errorMessage = bindingResult
                     .getFieldErrors()
                     .stream()
                     .map(FieldError::getDefaultMessage)
-                    .collect(Collectors.joining("<br>"));
+                    .collect(Collectors.joining("\n"));
 
+            model.addAttribute("errorMessage", errorMessage);
+            model.addAttribute("form", form);
 
-            return getWriteFormHtml(errorMessage, writeForm.title, writeForm.content);
+            return "post/post/write";
         }
-        Post post = postService.write(writeForm.title, writeForm.content);
 
-        return "%d 번 글이 생성 되었습니다.".formatted(post.getId());
-    }
+        Post post = postService.write(form.getTitle(), form.getContent());
 
-    private String getWriteFormHtml(
-            String errorMessage,
-            String title,
-            String content
-    ) {
-        return """
-                <div style="color:red;">%s</div>
-                <form action="/posts/doWrite" method="POST">
-                  <input type="text" name="title" placeholder="제목" value="%s" autofocus>
-                  <br>
-                  <textarea name="content" placeholder="내용">%s</textarea>
-                  <br>
-                  <input type="submit" value="작성">
-                </form>
-                """.formatted(errorMessage, title, content);
+        model.addAttribute("post", post);
+
+        return "post/post/list";
     }
 }
