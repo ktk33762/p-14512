1438677746af4dbb116cc1882ce656e64ea8d042
ktk3376 <ktk3376@gmail.com>
2026-05-12 14:23:53 +0900

    40강, 글 작성 시 작성자 정보 Principal 이용하여 저장

--- src/main/java/com/back/domain/member/controller/MemberController.java
+++ src/main/java/com/back/domain/member/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.back.domain.member.controller;
 
-import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
@@ -16,8 +15,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @RequestMapping("/members")
 @RequiredArgsConstructor
@@ -62,7 +59,7 @@ public class MemberController {
             return "member/member/register";
         }
 
-        Optional<Member> opMember = memberService.findByUsername(registerForm.getUsername());
+        memberService.findByUsername(registerForm.getUsername());
 
 
         try {
--- src/main/java/com/back/domain/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/service/MemberService.java
@@ -3,10 +3,9 @@ package com.back.domain.member.service;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -26,7 +25,8 @@ public class MemberService {
         return this.memberRepository.save(member);
     }
 
-    public Optional findByUsername(String username) {
-        return memberRepository.findByUsername(username);
+    public Member findByUsername(String username) {
+        return memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
     }
 }
